4201d4ce139205fa2f4f75983840985a82458ff6
Sebastian Salazar <sebastian.salazar.solano@gmail.com>
2025-02-12 16:03:28 -0600

    feat: add user dto typescript file

---
+++ src/modules/users/dtos/user.dto.ts
@@ -0,0 +1,12 @@
+export class CreateUserDto {
+    username: string;
+    email: string;
+    password: string;
+}
+
+export class UpdateUserDto {
+    username?: string;
+    email?: string;
+    password?: string;
+    balance?: number;
+}
--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,31 +1,31 @@
 import {
-  Entity,
-  PrimaryGeneratedColumn,
-  Column,
-  CreateDateColumn,
-  UpdateDateColumn,
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
 } from 'typeorm';
 
 @Entity({ name: 'users' })
 export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
+    @PrimaryGeneratedColumn()
+    id: number;
 
-  @Column({ unique: true })
-  username: string;
+    @Column({ unique: true })
+    username: string;
 
-  @Column({ unique: true })
-  email: string;
+    @Column({ unique: true })
+    email: string;
 
-  @Column()
-  password: string;
+    @Column()
+    password: string;
 
-  @Column({ type: 'decimal', default: 0 })
-  balance: number;
+    @Column({ type: 'decimal', default: 0 })
+    balance: number;
 
-  @CreateDateColumn()
-  createdAt: Date;
+    @CreateDateColumn()
+    createdAt: Date;
 
-  @UpdateDateColumn()
-  updatedAt: Date;
+    @UpdateDateColumn()
+    updatedAt: Date;
 }
