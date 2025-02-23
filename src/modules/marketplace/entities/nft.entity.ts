c77777a78040d8bc0a1b98994929fd0448c08136
Mario Fernando Araya Royo <arayaroyo686@gmail.com>
2025-02-22 18:44:51 -0600

    style: format NFT entity for improved readability

--- src/modules/marketplace/entities/nft.entity.ts
+++ src/modules/marketplace/entities/nft.entity.ts
@@ -1,34 +1,33 @@
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    CreateDateColumn,
-  } from 'typeorm';
-  
-  @Entity({ name: 'nfts' })
-  export class NFT {
-    @PrimaryGeneratedColumn()
-    id: number;
-  
-    @Column()
-    name: string;
-  
-    @Column({ type: 'text' })
-    description: string;
-  
-    @Column()
-    imageUrl: string;
-  
-    @Column()
-    ownerId: number;
-  
-    @Column({ default: false })
-    isListed: boolean;
-  
-    @Column({ type: 'float', default: 0 })
-    price: number;
-  
-    @CreateDateColumn()
-    createdAt: Date;
-  }
-  
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'nfts' })
+export class NFT {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column()
+  imageUrl: string;
+
+  @Column()
+  ownerId: number;
+
+  @Column({ default: false })
+  isListed: boolean;
+
+  @Column({ type: 'float', default: 0 })
+  price: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
