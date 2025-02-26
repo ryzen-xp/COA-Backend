9c07de520d05904c1f2fdf00d898686f2ad1dc66
Manasseh Onwe <onwe.manasseh@gmail.com>
2025-02-26 20:15:18 +0100

    documentation

--- src/modules/blockchain/dtos/transfer.dto.ts
+++ src/modules/blockchain/dtos/transfer.dto.ts
@@ -1,14 +1,27 @@
 import { IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class TransferDto {
+  @ApiProperty({
+    description: 'Source account address',
+    example: '0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef'
+  })
   @IsString()
   @IsNotEmpty()
   from: string;
 
+  @ApiProperty({
+    description: 'Destination account address',
+    example: '0xabcdef1234567890abcdef1234567890abcdef1234567890abcdef1234567890'
+  })
   @IsString()
   @IsNotEmpty()
   to: string;
 
+  @ApiProperty({
+    description: 'Token ID to transfer',
+    example: '1'
+  })
   @IsString()
   @IsNotEmpty()
   tokenId: string;
